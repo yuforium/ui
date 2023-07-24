c11d729e94c5c8fba90ce38a9248a803421b6f5a
Chris Moser <cpmoser@gmail.com>
2023-07-24 09:44:13 +0200

    feat: mvp - change user route to `users` to match api routes - better handling of login errors

--- projects/test-app/src/app/modules/auth/auth.component.ts
+++ projects/test-app/src/app/modules/auth/auth.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AuthService, Configuration } from 'projects/ui-common/src/lib/api';
 import { switchMap } from 'rxjs';
 import { UserService } from 'projects/ui-common/src/lib/api/api/user.service';
 import { AppService } from '../../app.service';
 import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {
+export class AuthComponent implements AfterViewInit {
   public loading = false;
+  public loginError = false;
+  public loginErrorMessage = '';
+  @ViewChild('username', {static: true}) usernameInput!: ElementRef<HTMLInputElement>;
+  @ViewChild('password', {static: true}) passwordInput!: ElementRef<HTMLInputElement>;
 
   constructor(
     protected appService: AppService,
@@ -20,6 +25,15 @@ export class AuthComponent {
     protected userService: UserService,
     protected router: Router
   ) { }
+
+  ngAfterViewInit(): void {
+    this.usernameInput.nativeElement.focus();
+    this.passwordInput.nativeElement.onkeyup = (e) => {
+      if (e.key === 'Enter') {
+        this.login(this.usernameInput.nativeElement.value, this.passwordInput.nativeElement.value);
+      }
+    };
+  }
 
   public get authenticated(): boolean {
     return this.appService.authenticated;
@@ -43,10 +57,19 @@ export class AuthComponent {
   }
 
   onLogin(response: any): void {
-    this.router.navigate(['/user', response.preferredUsername])
+    this.router.navigate(['/', 'users', response.preferredUsername])
   }
 
   onLoginError(err: any): void {
-    
+    if (err instanceof HttpErrorResponse && err.status === 401) {
+      this.loginErrorMessage = 'Invalid username or password';
+    }
+    else {
+      this.loginErrorMessage = 'Unable to login at this time';
+    }
+    this.loginError = true;
+
+    this.usernameInput.nativeElement.focus();
+    this.usernameInput.nativeElement.select();
   }
 }
